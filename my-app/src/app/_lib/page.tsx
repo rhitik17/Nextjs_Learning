f46a476af2a7da0b42d1da399da3d961b71a72ab
Rhitik_HBR <ritikbhandari17@gmail.com>
2024-08-21 23:12:05 +0545

    adding _lib folder as private folder

---
+++ my-app/src/app/_lib/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const privateRoute = () => {
+  return (
+    <div>You cannot view this in browser</div>
+  )
+}
+
+export default privateRoute
--- my-app/src/app/dashboard/line-chart.tsx
+++ my-app/src/app/dashboard/line-chart.tsx
@@ -6,4 +6,4 @@ const lineChart = () => {
   )
 }
 
-export default lineChart;
+export default lineChart; 
